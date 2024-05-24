c9281c416fedb9445b28000727002fdb9cf622e1
Aneeqa9007 <138521630+Aneeqa9007@users.noreply.github.com>
2024-05-24 16:33:10 +0530

    Add files via upload

---
+++ assignment 6.js
@@ -0,0 +1,24 @@
+// multiples of 3
+
+a=[]
+
+for(let i=1;i<=100;i++){
+    if(i%3==0){
+      a.push(i)     
+    }
+}
+
+
+
+console.log(a)
+
+// loop from 0 to 100
+function loop(){
+for(let j=0;j<=100;j++){
+    console.log(j)
+}
+}
+
+loop()
+
+
